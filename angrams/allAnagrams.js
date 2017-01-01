50ce59ab542c1eaebc76fbe1c6d5a2fcb8ba90b7
mohamed <toot2009@hotmail.com>
2017-01-01 10:14:16 +0200

    my tray

--- angrams/allAnagrams.js
+++ angrams/allAnagrams.js
@@ -15,7 +15,85 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
-	
+  var allAnagrams = function(string) {
+  //	var arr=string.split("")
+  var ciriar=[]
+  for(var i = 0 ; i<string.length;i++){
+  	for(var j=0;j<string.length;j++){
+  		for(var k=0;k<string.length;k++){
+  			ciriar.push(string[i]+string[j]+string[k])
+  			if(ciriar[i])
+  		}
 
+  }
+}
+return ciriar
 };
+
+
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                                      //  A2
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////
+// function allang(str){
+// 	if(str.length<2){
+// 		return str
+// 	}
+// 	var arr=[]
+// 	var world;
+// 	var selc;
+// 	var aft;
+// 	for(var i=0;i<str.length;i++){
+// 		 world=str[i]
+// 		 // selc=str.slice(0,i)
+// 		 // aft=str[i+1]
+// 		 // arr=allang(world,aft,selc)
+// 		 woo=str.substr(0,i)+str.supstr(i+1,str.length-1)
+// 		myin=allang(woo)
+// 		for(var j=0;j<myin.length;i++){
+// 			arr.push(world+myin[j])
+// 		}
+// 	}
+
+// 	return arr
+// }
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                                     //   from internet
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////
+  function allAnagrams (word) {
+  	if (word.length < 2) {
+  		return [word];
+  	} else {
+  		var allAnswers = [];
+  		for (var i = 0; i < word.length; i++) {
+  			var letter = word[i];
+  			var shorterWord = word.substr(0, i) + word.substr(i + 1, word.length - 1);
+  			var shortwordArray = allAnagrams(shorterWord);
+  			for (var j = 0; j < shortwordArray.length; j++) {
+  				allAnswers.push(letter + shortwordArray[j]);
+  			}
+  		}
+  		return allAnswers;
+  	}
+  }
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                                     //   ANOTHER TRAY 
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////
+  function all(str){
+  	var arr=[]
+  	for(var i=0;i<str.length;i++){
+  		var first=str[i]
+  		var  last=str[str.length-1]
+  		var midill=str[str.length/2+1]
+  		var ong=all(last+midill)
+  		for(var k=0;k<ong.length;k++){
+  			arr.push(first+ong[k])
+  		}
+  	}
+  	return arr
+  }
+
+
+
+
+
