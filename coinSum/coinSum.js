e9bf478faf74ad16240d194386596a4d17e8eaa9
mohamed <toot2009@hotmail.com>
2017-01-09 09:56:03 +0200

    not finshed

--- coinSum/coinSum.js
+++ coinSum/coinSum.js
@@ -12,3 +12,24 @@ Given that total amount of pences, calculate the number of ways to create that a
 Example:
 coinCombination(200p) //-> 73682
 */
+
+function coinCombination(number) {
+
+	var bound = "1p, 2p, 5p, 10p, 20p, 50p, 100p, 200p"
+      
+    //var index = bound.substring(number,-1)
+
+	var array = []
+	 
+	for(var i = 0 ; i < bound.length ; i++){///O(n)
+
+		if(bound.indexOf(number) >= 0){
+           /////////to equall number
+         ///i need to have new array from first index to the number 
+         //// i thot i shuld to have to for loop or recergen and slice the array rely idon't know
+
+		}
+
+	}
+	return array.join("")
+}
