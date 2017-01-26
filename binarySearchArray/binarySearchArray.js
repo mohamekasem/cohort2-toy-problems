5918dda7ce3b7758263d73fc0741fb67057522e5
mohamed <toot2009@hotmail.com>
2017-01-26 09:24:19 +0200

    binary

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,7 +10,35 @@
  * console.log(index); // 4
  */
 
-var binarySearch = function (array, target) {
-  
+ var binarySearch = function (array, target) {
+
+ 	 var middle = Math.floor(array.length/2);				// i will have some var here 
+
+ 	 if(target === middle){
+ 	 	return middle
+ 	};                            	// i whant to have new array from 0 to middl && to have from middl to end of array 
+ 									// i vwi to see if target is bigger or smaller than to mack the arr
+ 									//	i will eteret on the elment in the 
+ 									//i will chake if the elment = target i will return the length /
+ 		if(target < middle){
+ 			var first = array.slice(0,middle);
+ 			console.log(first)
+ 				for (var i = 0 ; i < first.length ; i++){
+ 					if(target === first[i]){
+ 					//	return some thing like indexOf(target)
+ 						return array.indexOf(first[i]) 
+ 					
+ 					}
+ 				}
+ 		};
+ 		if(target > middle){
+ 		var last = array.slice(middle,array.length);
+ 			for (var i = 0 ; i < last.length ; i++){
+ 				if(target === last[i]){
+ 					//	return some thing like indexOf(target)
+ 					return  array.indexOf(last[i]) 
+ 				}
+ 			}
+ 		}
 };
 
