1b69bb3f2ce62b998482f6af9d4f79c232697651
mohamed <toot2009@hotmail.com>
2017-01-31 09:25:10 +0200

    half solve

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,9 +7,33 @@
  * Extra credit: Make your function handle negative numbers.
  */
 
-var largestProductOfThree = function(array) {
-	
-};
+ var largestProductOfThree = function(array) {
+ 	//var arr = []
+ 	array.sort(function(a,b){
+    return	b-a
+ 	})
+ 	var x = array[0]*array[1]*array[2]
+ //	var y = 
+ 	return	 Math.max(x) //i will have anothur prametur he will be negative	
+ 	//return	x
+ }
+ // 	for(var i= 0;i< array.length; i++){
+ // 		//for(var j= 1; j<array.length; j++){
+ // 				arr.push(array[i]*array[i+1])
+ // 				console.log(arr)
+ 			
+ 			
+
+ // 		//}	
+ // 	}
+ // 	var max = arr[0];
+ // 	for(var s= 0; s<arr.length; s++){
+ // 		if(max>arr[s]){
+ // 			max = arr[s]
+ // 		}
+ // 	}
+ // 			return max;
+ // }
 
 
 
