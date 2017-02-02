0a1b1a79ef170c8c9a29245d3d41d1c6b5aff63a
mohamed <toot2009@hotmail.com>
2017-02-02 09:22:59 +0200

    issup

--- isSubsetOf/isSubsetOf.js
+++ isSubsetOf/isSubsetOf.js
@@ -15,13 +15,36 @@
  *
  * See http://en.wikipedia.org/wiki/Subset for more on the definition of a
  * subset.
-*/
+ */
 
 
 /*
  * Extra credit: Make the method work for arrays that contain objects and/or arrays as elements.
-*/
+ */
 
-Array.prototype.isSubsetOf = function (arr) {
-
+ Array.prototype.isSubsetOf = function (arr) {
+											//i whant to know how is the sup;
+											//i will have avariable to inclod the variable whith no duplc;
+											//mack som forloob to delet the duplicates
+ var array = [];
+											  // if(Array.prototype.length > arr){
+											  //i have proplem to how to get the elment from the array.pro;
+											  // }
+ 										 	  //console.log(Array.prototype)
+ var arrObj = Object.getOwnPropertyNames(Array.prototype);
+ 	for( var Key in arrObj ) {
+     console.log(arrObj[Key],Key);
+    }
+    for(var i= 0; i< Array.prototype.length; i++){
+  	 if(array.indexOf(Array.prototype[i])=== -1){
+  		array.push(Array.prototype[i])
+  		console.log(array)
+  	  }
+    }
+    for(var k = 0; k<array.length; k++){
+  	 if(arr.in(array[k])){
+  		return true;
+  	 }
+    }
+  return false;
 }
